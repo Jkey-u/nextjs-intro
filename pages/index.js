765447b90938a263a9fc159526f51a96ed95ea79
Jkey-u <dum6894@gmail.com>
2022-03-05 21:20:03 +0900

    #1.2 Static Pre Rendering - example

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-// framework vs library
-/*
-    - [library] : 우리가 원할 때 불르고, 원할 때 사용하는 것 (React.js)
-    - [framework] : 규칙을 따라서 올바른 위치에 직접 코드를 작성하면, framework가 코드를 불러서 사용 (Next.js)
-*/
+import { useState } from "react";
 
 export default function Home() {
-    return "Hi!";
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <h1>Hello {counter}</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}>+</button>
+    </div>
+  );
 }
